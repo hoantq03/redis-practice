c160b50e4868cbd3c7976142c7a7541857776cbd
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-04 15:47:29 +0700

    [DEV] using Lua script

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,12 +1,5 @@
-import { itemsKey, itemsByViewKey, usernamesUniqueKey, itemsViewKey } from '$services/keys';
 import { client } from '$services/redis';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const inserted = await client.pfAdd(itemsViewKey(itemId), userId);
-	if (inserted) {
-		return Promise.all([
-			await client.hIncrBy(itemsKey(itemId), 'views', 1),
-			await client.zIncrBy(itemsByViewKey(), 1, itemId)
-		]);
-	}
+	return client.incrementView(itemId, userId);
 };
